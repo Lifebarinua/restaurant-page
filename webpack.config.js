7efc7affa7ad50984c18d9ccf8808f65ca589cc1
Lifebarinua <barinualife@gmail.com>
2025-08-29 23:26:39 +0100

    add html file path to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // or 'production'
+  mode: 'development', // change to 'production' before deploying
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js', // or 'main.js', doesn't matter
     path: path.resolve(__dirname, 'dist'),
-    clean: true, // clears old build files
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      filename: 'index.html', // REQUIRED for GitHub Pages
+    }),
+  ],
   module: {
     rules: [
       {
@@ -16,19 +22,14 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,   // <--- add this
-        type: 'asset/resource',            // built-in file loader
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html', // your custom HTML
-    }),
-  ],
   devServer: {
     static: './dist',
     hot: true,
-    open: true, // auto open browser
+    open: true,
   },
 };
